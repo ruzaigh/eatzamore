acd657fa631b41f9cbd4e155eb0be11a2a3d89f9
Ruzaigh <ruzaigh.kalam2@gmail.com>
2024-08-21 22:25:07 +0200

    hamburger, restaurant and menu list complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {HamburgerComponent} from "./hamburger/hamburger.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, HamburgerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'eatzamore';
--- src/app/restaurants/ components/menu-list/menu-list.component.ts
+++ src/app/restaurants/ components/menu-list/menu-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { Dish } from '../../interface/restaurant';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-menu-list',
   standalone: true,
-  imports: [],
+  imports: [MatExpansionModule],
   templateUrl: './menu-list.component.html',
   styleUrl: './menu-list.component.css',
 })
 export class MenuListComponent {
   @Input() dishes: Dish[];
-
+  dishImageMap = {
+    'Appetizers/Starters': 'assets/starters-icon.png',
+    Salads: 'assets/salads-icon.png',
+    'Main Courses': 'assets/main-icon.png',
+    Seafood: 'assets/seafood-icon.png',
+  };
   constructor() {}
 }
