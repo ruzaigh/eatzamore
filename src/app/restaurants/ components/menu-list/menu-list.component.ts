5d52832579cd812d0452e34ceb25dec44ce788e3
Ruzaigh <ruzaigh.kalam2@gmail.com>
2024-08-21 20:18:17 +0200

    nav-bar complete

--- src/app/restaurants/ components/menu-list/menu-list.component.ts
+++ src/app/restaurants/ components/menu-list/menu-list.component.ts
@@ -1,20 +1,15 @@
-import {Component, Input} from '@angular/core';
-import {Dish, } from "../../interface/restaurant";
+import { Component, Input } from '@angular/core';
+import { Dish } from '../../interface/restaurant';
 
 @Component({
   selector: 'app-menu-list',
   standalone: true,
   imports: [],
   templateUrl: './menu-list.component.html',
-  styleUrl: './menu-list.component.css'
+  styleUrl: './menu-list.component.css',
 })
 export class MenuListComponent {
-  @Input() dishes: Dish[]
+  @Input() dishes: Dish[];
 
-  constructor() {
-
-  }
-
-
-
+  constructor() {}
 }
